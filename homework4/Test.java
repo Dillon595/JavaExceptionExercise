696923e2cc5081bad57fbad3328d97a04768f171
Alun <39848824+Dillon595@users.noreply.github.com>
2021-01-06 16:00:41 +0800

    Update Test.java

--- homework4/Test.java
+++ homework4/Test.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * @date 2021-01-06 13:42:18
 */
 
-public class Test extends Drink {
+public class Test extends Drink {	//因Drink类为抽象类不能实例化对象，所以此处测试类继承Drink，否则需多创建一个Drink的子类
 
 	public static void main(String[] args) {
 		Test test = new Test();
